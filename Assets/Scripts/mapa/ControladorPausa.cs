acb9ae2a7fec2f5041308396fc277802125a12a8
GorkaUrabayen <146164587+GorkaUrabayen@users.noreply.github.com>
2026-02-11 16:53:35 +0100

    Escena de derrota,nivel1 y de inicio actualizadas y boton de pausa
    He puesto que el fondo a la escala correcta y que escale con la pantalla del ordenador, tambien he puesto bien los botones ya que estaban regular

---
+++ Assets/Scripts/mapa/ControladorPausa.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControladorPausa : MonoBehaviour
+{
+    public GameObject panelPausa; 
+    public GameObject botonPausaUI;    // El botón de "Pause"
+    public GameObject botonReanudarUI; // El botón de "Play" que acabas de crear
+    private bool estaPausado = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (estaPausado) Reanudar();
+            else Pausar();
+        }
+    }
+
+    public void Pausar()
+    {
+        estaPausado = true;
+        panelPausa.SetActive(true);
+        
+        // INTERCAMBIO DE BOTONES
+        botonPausaUI.SetActive(false);    // Apagamos pausa
+        botonReanudarUI.SetActive(true);  // Encendemos reanudar
+        
+        Time.timeScale = 0f; 
+    }
+
+    public void Reanudar()
+    {
+        estaPausado = false;
+        panelPausa.SetActive(false);
+        
+        // INTERCAMBIO DE BOTONES
+        botonPausaUI.SetActive(true);     // Encendemos pausa
+        botonReanudarUI.SetActive(false); // Apagamos reanudar
+        
+        Time.timeScale = 1f; 
+    }
+
+    public void IrAlMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+}
